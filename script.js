93d93ae24766cc2352d96e7e54ccb07411dade03
Vishal Kumar <wwwvishalkumar872@gmail.com>
2025-06-27 21:02:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // Convert both strings to lowercase
+  const lowerS1 = s1.toLowerCase();
+  const lowerS2 = s2.toLowerCase();
+
+  // Use built-in indexOf to find the first occurrence
+  return lowerS1.indexOf(lowerS2);
 }
 
 // Please do not change the code below
